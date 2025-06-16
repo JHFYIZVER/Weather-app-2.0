d64dd406c38fa5e3c95b8d1c2f082163468e4849
Vadim <saraevv205@gmail.com>
2025-06-16 21:09:19 +0900

    add moment-timezone lib

--- src/entities/weather/highlights/lib/format-time.ts
+++ src/entities/weather/highlights/lib/format-time.ts
@@ -1,25 +1,15 @@
+import moment from 'moment-timezone';
 
 export default function formatTime(
   timestamp: number,
   timezoneOffset?: number
 ): string {
-  const date = new Date(timestamp * 1000);
   if (timezoneOffset !== undefined) {
-    const timezoneHours = timezoneOffset / 3600;
-    const timezoneString = `Etc/GMT${timezoneHours >= 0 ? "-" : "+"}${Math.abs(
-      timezoneHours
-    )}`;
-    return date.toLocaleTimeString("ru-RU", {
-      hour: "2-digit",
-      minute: "2-digit",
-      timeZone: timezoneString,
-      hour12: false,
-    });
+    return moment.unix(timestamp)
+      .utcOffset(timezoneOffset / 60)
+      .format('HH:mm');
   }
-  return date.toLocaleTimeString("ru-RU", {
-    hour: "2-digit",
-    minute: "2-digit",
-    timeZone: "Europe/Moscow",
-    hour12: false,
-  });
+  return moment.unix(timestamp)
+    .tz('Europe/Moscow')
+    .format('HH:mm');
 }
