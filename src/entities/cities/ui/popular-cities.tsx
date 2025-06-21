eb9e5cbb3ec78281870386677f7bec4be5c41887
Vadim <saraevv205@gmail.com>
2025-06-21 13:56:53 +0900

    remove framer-motion

--- src/entities/cities/ui/popular-cities.tsx
+++ src/entities/cities/ui/popular-cities.tsx
@@ -19,14 +19,13 @@ const PopularCities = () => {
       <CardTitle className="font-bold">Популярные города</CardTitle>
       <CardContent className="space-y-6 flex flex-col w-full px-0">
         {cities.map((city) => (
-          <Button className="my-2 relative" key={city.id}>
-            <Link
-              className="before:absolute before:inset-0 before:z-0"
-              href={`/?city=${city.name}`}
-            >
-              {city.name}
-            </Link>
-          </Button>
+          <Link
+            key={city.id}
+            className="my-2 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive relative before:absolute before:inset-0 before:z-0 bg-primary-foreground border text-primary hover:bg-primary-foreground/30 h-9 px-4 py-2 has-[>svg]:px-3"
+            href={`/?city=${city.name}`}
+          >
+            {city.name}
+          </Link>
         ))}
       </CardContent>
     </Card>
